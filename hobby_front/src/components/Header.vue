10998b907d8b0c5301a6f2ec8c5ede11b9961911
Lee Jiseon <wiahawm7@gmail.com>
2019-10-14 17:37:04 +0900

    S1P3151007-10 Create Header

---
+++ hobby_front/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-toolbar fixed>
+      <v-container>
+        <v-row>
+          <v-col md="2">
+            <v-toolbar-title>하하호호</v-toolbar-title>
+          </v-col>
+
+          <v-col md="3">
+            <v-btn text>모임</v-btn>
+            <v-btn text>커뮤니티</v-btn>
+          </v-col>
+          
+          <v-col md="2" offset-md="5">
+            <v-btn text>로그인</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
