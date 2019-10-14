a31a85df2c465c556688efb29eabc9b3c543e605
dongsik93 <ehdtlr9376@naver.com>
2019-10-14 15:03:17 +0900

    S1P3151007-4 Create accounts model

---
+++ hobby_back/accounts/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+# Create your models here.
+
+class User(models.Model):
+    userName = models.CharField(max_length=100)
+    userSex = models.CharField(max_length=20)
+    userAge = models.IntegerField()
+    userImage = models.ImageField(blank=True)
+    userGrade = models.IntegerField()
+
+    def __srt__(self):
+        return self.userName
+
+class PostOnetone(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    contents = models.TextField()
+    answer = models.TextField(blank=True)
+
+# class Image(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     file = ProcessedImageField(
+#             upload_to='accounts/images', # 저장위치
+#             processors=[ResizeToFill(600,600)], # 크기 지정
+#             format='JPEG', # 확장자 변경
+#             options={'quality':90}, # 원본의 90퍼센트 품질로 저장
+#     	)
+
+# 카테고리 대분류
+class Section(models.Model):
+    sectionName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sectionName
+
+# 카테고리 소분류
+class Group(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    groupName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.groupName
