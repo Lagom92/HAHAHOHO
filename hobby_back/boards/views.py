7652b7fffb3689ecef1722749a9963b47d4afc40
hyunsang <happylovetkd@naver.com>
2019-10-15 11:01:42 +0900

    postfree viewset

--- hobby_back/boards/views.py
+++ hobby_back/boards/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import PostFreeserializer
+from .models import PostFree
 # Create your views here.
+
+
+class PostFreeViewSet(viewsets.ModelViewSet):
+    queryset = PostFree.objects.all()
+    serializer_class = PostFreeserializer
+
+
+
