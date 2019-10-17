0e4d34af482c06a73244da883d63e86d90b11715
KimHoon <gnsl8391@gmail.com>
2019-10-17 17:47:10 +0900

    S1P3151007-22 Detail page 구조잡는중

---
+++ hobby_front/src/views/Detail.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col cols="9">
+                    <p>카테고리</p>
+                    <h1>글제목</h1>
+                    <h3>소제목</h3>
+                    <div>
+                        <h3>2019-10-30, 8명, 광주 서구</h3>
+                    </div>
+                </v-col>
+                <v-col cols="3">
+                    <button>참여 신청</button>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="7">
+                    <v-card class="excard">사진</v-card>
+                    <v-card class="excard">상세내용</v-card>
+                    <v-card class="excard">활동내용</v-card>
+                </v-col>
+                <v-col cols="4" offset="1">
+                    <v-card class="excard">성별, 연령, 상세위치, 비용 등..</v-card>
+                    <v-card class="excard">참여멤버 목록</v-card>
+                    <v-card class="excard">지도</v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Detail'
+}
+</script>
+
+<style>
+.excard {
+    height: 300px;
+    margin-bottom: 50px;
+}
+</style>
