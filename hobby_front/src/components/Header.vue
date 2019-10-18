0338f05d7b2ecb3ab1cad33b0aa7ebe4c8d4428f
Lee Jiseon <wiahawm7@gmail.com>
2019-10-18 17:32:08 +0900

    S1P3151007-5 kakao login axios 요청

--- hobby_front/src/components/Header.vue
+++ hobby_front/src/components/Header.vue
@@ -25,11 +25,14 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Header',
   data () {
     return {
       dialog: false,
+      accesstoken: '',
     }
   },
   methods: {
@@ -38,44 +41,18 @@ export default {
       Kakao.Auth.createLoginButton({
         container: '#kakao-login-btn',
         success: function(authObj) {
-          console.log(authObj);
-          /* 요청사항입니다.
-            1. http://127.0.0.1:8000/accounts/rest-auth/kakao/ 여기에 post요청
-              1-1. return으로 jwt토큰을 저장(jwt토큰은 사용자 인증에 사용)
-            2. http://127.0.0.1:8000/accounts/kakaoLogin/
-              2-1. return으로 유저 정보를 사용해서 유저페이지 꾸미기
-          */
-         axios.post('http://127.0.0.1:8000/accounts/rest-auth/kakao/', {access_token : accesstoken})
-         .then(res => {
-           console.log(res.data)
-         })
+          this.accesstoken = authObj.access_token;
+          axios.post('http://127.0.0.1:8000/accounts/rest-auth/kakao/', {body: this.accesstoken})
+          .then((res) => {
+            console.log(res)
+          }).catch((e) => {
+            console.log(e)
+          })
         },
         fail: function(err) {
           alert(JSON.stringify(err));
         }
       });
-    },
-    naverlogin() {
-      var naver_id_login = new naver_id_login("IaquHgHTmPlf_gc_a8es", "http://localhost:8080/callback");
-      var state = naver_id_login.getUniqState();
-      naver_id_login.setButton("white", 2,40);
-      naver_id_login.setDomain("http://localhost:8080/");
-      naver_id_login.setState(state);
-      naver_id_login.setPopup();
-      naver_id_login.init_naver_id_login();
-    },
-    naverlogincallback() {
-      var naver_id_login = new naver_id_login("IaquHgHTmPlf_gc_a8es", "http://127.0.0.1:8000/account/rest-auth/naver/callback");
-      // 접근 토큰 값 출력
-      alert(naver_id_login.oauthParams.access_token);
-      // 네이버 사용자 프로필 조회
-      naver_id_login.get_naver_userprofile("naverSignInCallback()");
-      // 네이버 사용자 프로필 조회 이후 프로필 정보를 처리할 callback function
-      function naverSignInCallback() {
-        alert(naver_id_login.getProfileData('email'));
-        alert(naver_id_login.getProfileData('nickname'));
-        alert(naver_id_login.getProfileData('age'));
-      }
     }
   }
 }
