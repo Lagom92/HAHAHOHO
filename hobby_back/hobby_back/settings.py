4b1a55fe0ec607c9b2535ac8169077ff6219c41e
dongsik93 <ehdtlr9376@naver.com>
2019-10-18 14:46:05 +0900

    S1P3151007-5 Complete Kakao Login

--- hobby_back/hobby_back/settings.py
+++ hobby_back/hobby_back/settings.py
@@ -147,6 +147,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
