3f7c412ae6c5f9f207da337eeef5aacbba038229
Yang Siyeong <jyyt0147@gmail.com>
2019-10-22 13:47:20 +0900

    S1P3151007-39 Create API document

--- hobby_back/boards/views.py
+++ hobby_back/boards/views.py
@@ -9,48 +9,166 @@ from rest_framework.response import Response
 
 
 class postHobby_list(generics.ListCreateAPIView):
+    '''
+    취미 게시판 list page를 생성, 조회 하는 API
+
+    ---
+    ## `/boards/hobby/`
+    ## 내용
+        - title: 게시판 제목
+        - contents: 게시판 내용
+        - startDate: 모임 시작시간
+        - gender: 모임 성별 여부
+        - age: 연령대
+        - member: 총 인원
+        - location: 모임을 할 장소
+        - fee: 비용
+    '''
     search_fields = ['title', 'contents', 'location']
     filter_backends = (filters.SearchFilter,)
     queryset = PostHobby.objects.all()
     serializer_class = PostHobbySerializer
     
 class postHobby_detail(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    취미 게시판 detail page를 조회, 수정, 삭제 하는 API
+
+    ---
+    ## `/boards/hobby/{id}/`
+    ## 내용
+        - title: 게시판 제목
+        - contents: 게시판 내용
+        - startDate: 모임 시작시간
+        - gender: 모임 성별 여부
+        - age: 연령대
+        - member: 총 인원
+        - location: 모임을 할 장소
+        - fee: 비용
+    '''
     queryset = PostHobby.objects.all()
     serializer_class = PostHobbySerializer
 
 # hobby게시판의 img list
 class img_list(generics.ListCreateAPIView):
+    '''
+    취미 게시판의 이미지들을 생성, 조회 하는 API
+    
+    ---
+    
+    ## 내용
+        - photo: 이미지
+        
+    '''
     queryset = HobbyImage.objects.all()
     serializer_class = ImgSerializer
 
 # hobby게시판의 img detail
 class img_detail(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    취미 게시판의 이미지들을 생성, 조회 하는 API
+    
+    ---
+    
+    ## 내용
+        - photo: 이미지
+        
+    '''
     queryset = HobbyImage.objects.all()
     serializer_class = ImgSerializer
 
 class postFree_list(generics.ListCreateAPIView):
+    '''
+    자유 게시판 list page를 생성, 조회 하는 API
+    
+    ---
+    ## `/boards/free/`
+    ## 내용
+        - title: 게시판 제목
+        - contents: 게시판 내용
+        - startDate: 모임 시작 시간
+        - created_at: 게시판 작성 시간
+        - group: 카테고리 소분류
+        - user: 게시판 작성자
+    '''
     search_fields = ['title', 'contents']
     filter_backends = (filters.SearchFilter,)
     queryset = PostFree.objects.all()
     serializer_class = PostFreeSerializer
 
 class postFree_detail(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    자유 게시판 detail page를 조회, 수정, 삭제 하는 API
+    
+    ---
+    ## `/boards/free/{id}/`
+    ## 내용
+        - title: 게시판 제목
+        - contents: 게시판 내용
+        - startDate: 모임 시작 시간
+        - created_at: 게시판 작성 시간
+        - group: 카테고리 소분류
+        - user: 게시판 작성자
+    '''
     queryset = PostFree.objects.all()
     serializer_class = PostFreeSerializer
 
 class notice_list(generics.ListCreateAPIView):
+    '''
+    공지사항 게시판 list page를 생성, 조회 하는 API
+    
+    ---
+    ## `/boards/notice/`
+    ## 내용
+        - title: 게시판 제목
+        - name: 게시판 작성자
+        - contents: 게시판 내용
+        - created_at: 게시판 작성 시간
+    '''
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
 
 class notice_detail(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    공지사항 게시판 detail page를 조회, 수정, 삭제 하는 API
+    
+    ---
+    ## `/boards/notice/{id}/`
+    ## 내용
+        - title: 게시판 제목
+        - name: 게시판 작성자
+        - contents: 게시판 내용
+        - created_at: 게시판 작성 시간
+    '''
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
 
 class faq_list(generics.ListCreateAPIView):
+    '''
+    FAQ 게시판 list page를 생성, 조회 하는 API
+    
+    ---
+    ## `/boards/faq/`
+    ## 내용
+        - title: 게시판 제목
+        - name: 게시판 작성자
+        - contents: 게시판 내용
+        - created_at: 게시판 작성 시간
+    '''
     queryset = Faq.objects.all()
     serializer_class = FaqSerializer
 
 class faq_detail(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    FAQ 게시판 detail page를 조회, 수정, 삭제 하는 API
+    
+    ---
+    ## `/boards/faq/{id}/`
+    ## 내용
+        - title: 게시판 제목
+        - name: 게시판 작성자
+        - contents: 게시판 내용
+        - created_at: 게시판 작성 시간
+    '''
     queryset = Faq.objects.all()
     serializer_class = FaqSerializer
 
--- hobby_back/hobby_back/urls.py
+++ hobby_back/hobby_back/urls.py
@@ -1,31 +1,21 @@
-"""hobby_back URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_jwt.views import obtain_jwt_token
+from .yasg import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('boards/', include('boards.urls')),
     path('accounts/', include('accounts.urls')),
     path('api-token-auth/', obtain_jwt_token),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('boards/', include('boards.urls')),
+
+    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
