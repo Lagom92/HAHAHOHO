603a731d7d5edd9047d371b582f38078bb9e22b5
Yang Siyeong <jyyt0147@gmail.com>
2019-10-22 16:58:26 +0900

    S1P3151007-40 Add django-debug-toolbar

--- hobby_back/hobby_back/urls.py
+++ hobby_back/hobby_back/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from rest_framework_jwt.views import obtain_jwt_token
 from .doc import *
@@ -23,3 +24,9 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 admin.site.site_header = "HAHAHOHO 관리자"
 admin.site.site_title = "HAHAHOHO 관리자 페이지"
 admin.site.index_title = "Welcome to HAHAHOHO"
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] 
