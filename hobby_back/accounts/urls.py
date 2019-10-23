780e896a777a0a002f6663ddd8ba86163ecc4674
dongsik93 <ehdtlr9376@naver.com>
2019-10-23 15:16:13 +0900

    CodeLint

--- hobby_back/accounts/urls.py
+++ hobby_back/accounts/urls.py
@@ -6,13 +6,13 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('rest-auth/kakao/',  KakaoLogin.as_view(), name='kakao_login'),
-    path('rest-auth/naver/',  NaverLogin.as_view(), name='naver_login'),
-    path('userSave/', views.userSave),
-    path('userInfo/', views.userInfo),
-    path('naverLogin/', views.Naver_Login),
-    path('kakaoPay/', views.kakaoPay),
-    path('edit/<int:id>/', views.editUser),
-    path('follow/<int:pk>/', views.follow_detail),
-    path('follow/', views.follow_list),
+    path('rest-auth/kakao',  KakaoLogin.as_view(), name='kakao_login'),
+    path('rest-auth/naver',  NaverLogin.as_view(), name='naver_login'),
+    path('userSave', views.userSave),
+    path('userInfo', views.userInfo),
+    path('naverLogin', views.Naver_Login),
+    path('kakaoPay', views.kakaoPay),
+    path('<int:id>', views.editUser),
+    path('follow/<int:pk>', views.follow_detail),
+    path('follow', views.follow_list),
 ]
--- hobby_back/hobby_back/urls.py
+++ hobby_back/hobby_back/urls.py
@@ -1,23 +1,23 @@
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
+# from rest_framework_jwt.views import obtain_jwt_token
 from .doc import schema_view
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('boards/', include('boards.urls')),
     path('accounts/', include('accounts.urls')),
-    path('api-token-auth/', obtain_jwt_token),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    # path('api-token-auth/', obtain_jwt_token),
+    # path('rest-auth/', include('rest_auth.urls')),
+    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
 
-    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
+    # path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
