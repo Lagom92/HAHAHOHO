c4648d3f9f404331ade3327d7dbde7010df02a13
Jo Hogeun <jhk7767@naver.com>
2019-10-28 10:42:44 +0900

    CodePolishing 인덴트 정리 및 코드 구조 통일

--- hobby_front/src/views/ListPage.vue
+++ hobby_front/src/views/ListPage.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>
-        <TopBanner></TopBanner>
-        <v-container>
-            <v-row>
-                <h1>모임 목록</h1>
-                <v-btn class="ml-auto" text to="/list/createmeeting">글쓰기</v-btn>
-            </v-row>
-            <v-layout>
-                <v-flex>
-                    <MeetingList></MeetingList>    
-                </v-flex>
-            </v-layout>
-        </v-container>
-    </div>
+  <div>
+    <TopBanner></TopBanner>
+    <v-container>
+      <v-row>
+        <h1>모임 목록</h1>
+        <v-btn class="ml-auto" text to="/list/createmeeting">글쓰기</v-btn>
+      </v-row>
+      <v-layout>
+        <v-flex>
+          <MeetingList></MeetingList>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -20,14 +20,10 @@ import TopBanner from '../components/TopBanner'
 import MeetingList from '../components/MeetingList'
 
 export default {
-    name: 'ListPage',
-    components: {
-        MeetingList,
-        TopBanner
-    }
+  name: 'ListPage',
+  components: {
+    MeetingList,
+    TopBanner
+  }
 }
 </script>
-
-<style>
-
-</style>
