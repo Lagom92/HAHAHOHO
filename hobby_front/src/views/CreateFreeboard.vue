7d9b5d71fac062b060fc4f412a8e592b71ff5a90
Lee Jiseon <wiahawm7@gmail.com>
2019-10-30 09:58:50 +0900

    S1P3151007-58 Freeboard 글쓰기 페이지 구조잡기

---
+++ hobby_front/src/views/CreateFreeboard.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col cols="12" md="6" offset-md="3">
+                    <div>
+                        <h1>자유게시판 글쓰기</h1>
+                    </div>
+                    <v-divider></v-divider>
+                    <v-form ref="form" v-model="valid">
+                        <v-text-field
+                        v-model="title"
+                        :counter="10"
+                        label="글 제목"
+                        ></v-text-field>
+
+                        <v-textarea
+                        outlined
+                        v-model="contents"
+                        label="글 내용"
+                        ></v-textarea>
+                    </v-form>
+                </v-col>
+            </v-row>
+            <v-row>
+              <div class="ml-auto">
+                <v-btn text color="primary">
+                  등록하기
+                </v-btn>
+              </div>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CreateFreeboard',
+    data () {
+        return {
+        title: '',
+        content: '',
+        valid: true,
+        }
+    }
+}
+</script>
