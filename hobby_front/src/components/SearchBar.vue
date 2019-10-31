edd84521d73d64fc8e7fcd6624b78ea69ad807a6
Yang Siyeong <jyyt0147@gmail.com>
2019-10-31 16:26:36 +0900

    S1P3151007-66 Develop search function

--- hobby_front/src/components/SearchBar.vue
+++ hobby_front/src/components/SearchBar.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
     <v-card elevation="0" color="#00ff0000">
-      <v-text-field
+      <!-- <v-text-field
       label="Search"
       append-outer-icon="mdi-magnify"
       single-line
-      ></v-text-field>
+      
+      ></v-text-field> -->
+      검색: 
+      <input 
+        v-model="word" 
+        placeholder="여기를 수정해보세요" 
+        @keyup.enter="search()"
+      >
+
     </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchBar'
+  name: 'SearchBar',
+  data () {
+    return {
+      posts: [],
+      word: ''
+    }
+  },
+  methods: {
+    search: function () {
+      const baseUrl = this.$store.state.baseUrl
+      console.log(this.word)
+      const apiUrl = baseUrl + 'boards/hobby?q=' + this.word
+      this.$http.get(apiUrl)
+        .then(res => {
+          console.log(res)
+          this.posts = res.data
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
