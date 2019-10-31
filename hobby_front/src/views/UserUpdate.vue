4e3baebe76650052aba70691285ebc5881d31e38
Lee Jiseon <wiahawm7@gmail.com>
2019-10-31 11:32:55 +0900

    S1P3151007-65 Userupdate page 디자인

--- hobby_front/src/views/UserUpdate.vue
+++ hobby_front/src/views/UserUpdate.vue
@@ -2,49 +2,33 @@
   <div>
     <v-container>
       <v-row>
-        <v-col cols="6" offset="3">
+        <v-col cols="12" md="8" offset-md="2">
           <div>
             <h1>정보수정</h1>
           </div>
-          <v-form
-          ref="form"
-          v-model="valid"
-          lazy-validation
-          >
+          <v-divider class="ma-3"></v-divider>
+
+          <v-file-input accept="image/*" label="프로필 사진"></v-file-input>
+
+          <v-form ref="form" v-model="valid" >
+            <v-text-field
+            v-model="name"
+            :counter="10"
+            :rules="nameRules"
+            label="닉네임"
+            required
+            ></v-text-field>
+            <v-text-field
+            v-model="address"
+            :rules="addressRules"
+            label="주요 활동 지역"
+            required
+            ></v-text-field>
+            <p class="mb-0">선호카테고리</p>
             <v-row>
-              <v-col cols="3">
-                <h3>이름</h3>
-              </v-col>
-              <v-col cols="9">
-                <v-text-field
-                v-model="name"
-                :counter="10"
-                :rules="nameRules"
-                label="Name"
-                required
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="3">
-                <h3>주소</h3>
-              </v-col>
-              <v-col cols="9">
-                <v-text-field
-                v-model="address"
-                label="Address"
-                required
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="3">
-                <h3>선호카테고리</h3>
-              </v-col>
-              <v-col cols="9">
+              <v-col cols="12">
                 <v-card
                   class="mx-auto"
-                  max-width="500"
                 >
                   <v-card-title class="title font-weight-regular justify-space-between">
                     <span>{{ currentTitle }}</span>
@@ -59,10 +43,10 @@
                   <v-window v-model="step">
                     <v-window-item :value="1">
                       <v-card
-                        max-width="400"
+                        elevation="0"
                         class="mx-auto"
                       >
-                        <v-container class="pa-1">
+                        <v-container class="px-5">
                           <v-item-group
                             v-model="selected"
                             multiple
@@ -72,23 +56,23 @@
                                 v-for="(card, i) in cards"
                                 :key="i"
                                 cols="12"
-                                md="6"
+                                md="4"
                               >
                                 <v-card>
                                   <v-item v-slot:default="{ active, toggle }">
                                     <v-img
                                       :src="card.img"
-                                      class="white--text align-end"
+                                      class="white--text align-center"
                                       height="150"
                                       gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
                                       @click="toggle"
                                     >
-                                      <v-card-title v-text="card.title"></v-card-title>
-                                      <v-btn icon dark >
+                                      <v-btn icon dark class="mb-10">
                                         <v-icon>
                                           {{ active ? 'mdi-heart' : 'mdi-heart-outline' }}
                                         </v-icon>
                                       </v-btn>
+                                      <v-card-title v-text="card.title" class="justify-center"></v-card-title>
                                     </v-img>
                                   </v-item>
                                 </v-card>
@@ -101,7 +85,7 @@
 
                     <v-window-item :value="2">
                       <v-card-text 
-                        v-for="category in categoryList"
+                        v-for="(sub,category) in CategoryList"
                         :key="category">
                         <h3 class="title mb-2">{{category}}</h3>
 
@@ -112,26 +96,13 @@
                           <v-chip 
                             filter 
                             outlined
-                            v-for="sub in subCategoryList"
-                            :key="sub"
+                            v-for="s in sub"
+                            :key="s"
                           >
-                            {{sub}}
+                            {{s}}
                           </v-chip>
                         </v-chip-group>
                       </v-card-text>
-                    </v-window-item>
-
-                    <v-window-item :value="3">
-                      <div class="pa-4 text-center">
-                        <v-img
-                          class="mb-4"
-                          contain
-                          height="128"
-                          src="https://cdn.vuetifyjs.com/images/logos/v.svg"
-                        ></v-img>
-                        <h3 class="title font-weight-light mb-2">Welcome to Vuetify</h3>
-                        <span class="caption grey--text">Thanks for signing up!</span>
-                      </div>
                     </v-window-item>
                   </v-window>
 
@@ -147,7 +118,7 @@
                     </v-btn>
                     <v-spacer></v-spacer>
                     <v-btn
-                      :disabled="step === 3"
+                      :disabled="step === 2"
                       color="primary"
                       depressed
                       @click="nextstep()"
@@ -158,26 +129,8 @@
                 </v-card>
               </v-col>
             </v-row>
-            <v-checkbox
-            v-model="checkbox"
-            :rules="[v => !!v || 'You must agree to continue!']"
-            label="Do you agree?"
-            required
-            ></v-checkbox>
-            <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="validate"
-            >
-              수정
-            </v-btn>
-            <v-btn
-            color="error"
-            class="mr-4"
-            @click="reset"
-            >
-              초기화
+            <v-btn color="error" >
+              저장하기
             </v-btn>
           </v-form>
         </v-col>
@@ -197,34 +150,37 @@ export default {
         v => (v && v.length <= 10) || 'Name must be less than 10 characters'
       ],
       address: '',
-      select: null,
-      checkbox: false,
+      addressRules: [
+        v => !!v || 'Address is required',
+      ],
       cards: [
         { 
           title: '스포츠', 
           img: 'https://cdn.pixabay.com/photo/2015/01/26/22/40/child-613199_1280.jpg',
-          sub: ['등산', '캠핑','해외여행','국내여행']
+          sub: ['등산', '캠핑','볼링','축구','배구','배드민턴','자전거','탁구']
         },
         { 
           title: '여행', 
           img: 'https://cdn.pixabay.com/photo/2015/07/11/23/02/plane-841441_1280.jpg',
-          sub: ['1', '2','3','4']  
+          sub: ['국내', '해외','낚시']  
         },
         { title: '공연/전시회', 
           img: 'https://cdn.pixabay.com/photo/2016/11/23/15/48/audience-1853662_1280.jpg',
-          sub: ['11', '22','33','44']  
+          sub: ['영화', '뮤지컬','연극','콘서트']  
         },
         { title: '독서/인문학', 
           img: 'https://cdn.pixabay.com/photo/2016/01/19/17/53/books-1149959_1280.jpg',
-          sub: ['12', '23','34','45']
+          sub: ['책', '시사','글쓰기','인문학']
+        },
+        { title: '음악/악기', 
+          img: 'https://cdn.pixabay.com/photo/2015/07/31/15/01/guitar-869217_1280.jpg',
+          sub: ['노래', '기타','피아노','드럼']
         },
       ],
       selected: [],
-      loading: false,
       search: '',
       step: 1,
-      categoryList: [],
-      subCategoryList: []
+      CategoryList: {}
     }
   },
   methods: {
@@ -233,21 +189,14 @@ export default {
         this.snackbar = true
       }
     },
-    reset () {
-      this.$refs.form.reset()
-    },
     backstep() {
       this.step--
-      this.categoryList = []
-      this.subCategoryList = []
+      this.CategoryList = {}
     },
     nextstep() {
       this.step++
       for (var i of this.selected) {
-        this.categoryList.push(this.cards[i].title)
-        for (var j of this.cards[i].sub) {
-          this.subCategoryList.push(j)
-        }
+        this.CategoryList[this.cards[i].title] = this.cards[i].sub
       }      
     }
   },
@@ -277,14 +226,8 @@ export default {
         switch (this.step) {
           case 1: return '대분류'
           case 2: return '소분류'
-          default: return 'Account created'
         }
       }
-  },
-  watch: {
-    selected () {
-      this.search = ''
-    }
   }
 }
 </script>
