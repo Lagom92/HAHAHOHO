56bfde8a01ecfb33dcb2d07b35e8bc5f2856b66d
Lee Jiseon <wiahawm7@gmail.com>
2019-11-01 17:51:41 +0900

    S1P3151007-71 MeetingList page 디자인 수정

--- hobby_front/src/components/Meeting.vue
+++ hobby_front/src/components/Meeting.vue
@@ -15,39 +15,37 @@
             :src= "data.photo"
             height="250px"
             class="text-right pa-2"
-            @click="toggle"
             >
-              <v-btn icon dark color="#F3B749">
+              <v-btn icon dark color="#ff4e50" @click="toggle">
                 <v-icon>
                   {{ active ? 'mdi-heart' : 'mdi-heart-outline' }}
                 </v-icon>
               </v-btn>
             </v-img>
           </v-item>
-          <v-card-text class="pt-6" style="position: relative;">
+          <v-card-text class="plusbtn pt-6">
             <v-btn
             absolute
-            color="#F3B749"
+            color="#EE7785"
             class="white--text"
             fab
             small
             right
             top
             :to="'/list/detail/' + data.id"
-            
             >
               <v-icon>mdi-plus</v-icon>
             </v-btn>
             <div class="mb-2" >
-              <v-chip small color="#9AB878" dark>{{data.subclassname}}</v-chip>
+              <v-chip x-small color="#74b4a0" dark>{{data.subclassname}}</v-chip>
+              <h3 class="meetingTitle title mb-2">{{data.title}}</h3>
             </div>
-            <h3 class="headline mb-2" color="#F3B749">{{data.title}}</h3>
-            <v-divider class="my-3"></v-divider>
+            <v-divider class="my-2"></v-divider>
             <v-row class="pl-3">
               <v-col class="pa-0">
-                <v-icon small>mdi-clock-outline</v-icon> {{data.startDay}}
+                <v-icon small>mdi-clock-outline</v-icon>{{data.startDay}}
               </v-col>
-              <v-col class="pa-0">
+              <v-col class="meetingLocation pa-0">
                 <v-icon small>mdi-map-marker</v-icon> {{data.location}}
               </v-col>
             </v-row>
@@ -71,3 +69,18 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+.meetingTitle
+  text-overflow ellipsis
+  white-space nowrap
+  overflow hidden
+
+.meetingLocation
+  text-overflow ellipsis
+  white-space nowrap
+  overflow hidden
+
+.plusbtn
+  position relative
+</style>
