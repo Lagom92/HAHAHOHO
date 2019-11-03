b9333c4214b52bc412e1d30769fcd2fa7bda3d8b
wiahawm <wiahawm@gmail.com>
2019-11-03 17:18:49 +0900

    main page 디자인

--- hobby_front/src/components/Footer.vue
+++ hobby_front/src/components/Footer.vue
@@ -1,12 +1,32 @@
 <template>
   <v-footer dark padless>
-    <v-card flat tile width="100%" class="text-center" >
-      <v-card-text>
+    <v-card flat tile width="100%"  >
+      <v-card-text class="text-center">
         하하호호 즐거운 취미생활
       </v-card-text>
-      <v-divider></v-divider>
+      <v-divider class="mx-4" inset></v-divider>
       <v-card-text>
-        2019 — <strong>SSAFY</strong>
+          <v-row>
+            <v-col cols="3" offset="3">
+              <p class="ma-0">
+                <span class="font-weight-bold">공동대표</span> &nbsp; 문동식, 이지선, 김훈, 양시영, 조호근
+              </p>
+              <p class="ma-0">
+                <span class="font-weight-bold">주소</span> &nbsp; 광주광역시 서구 
+              </p>
+            </v-col>
+            <v-col cols="3">
+              <p class="ma-0">
+                <span class="font-weight-bold">전화</span> &nbsp; 010-3143-9376
+              </p>
+              <p class="ma-0">
+                <span class="font-weight-bold">이메일</span> &nbsp; ehdtlr9376@gmail.com
+              </p>
+            </v-col>
+            <v-col cols="12" class="text-center">
+              © 2019 — <strong>하하호호</strong>
+            </v-col>
+          </v-row>
       </v-card-text>
     </v-card>
   </v-footer>
@@ -17,13 +37,7 @@ export default {
   name: 'Footer',
   data () {
     return {
-      icons: [
-        'fab fa-facebook',
-        'fab fa-twitter',
-        'fab fa-google-plus',
-        'fab fa-linkedin',
-        'fab fa-instagram'
-      ]
+      //
     }
   },
   components: {
--- hobby_front/src/components/SearchBar.vue
+++ hobby_front/src/components/SearchBar.vue
@@ -1,15 +1,9 @@
 <template>
-  <div class="text-center">
-    <v-card elevation="0" color="#00ff0000">
-      <v-text-field
-      label="Search"
-      v-model="word" 
-      append-outer-icon="mdi-magnify"
-      single-line
-      @keyup.enter="search()"
-      ></v-text-field>      
-    </v-card>
+<div class="text-center">
+  <div class="search-box">
+      <input v-model="word" type="text" @keyup.enter="search()" placeholder=" " /><span></span>
   </div>
+</div>
 </template>
 
 <script>
@@ -24,12 +18,84 @@ export default {
     search: function () {
       this.$store.commit('wordSave', this.word)
       var nowPosition = this.$router.history.current.name
+      var searchBox = document.querySelectorAll('.search-box input[type="text"] + span');
+
+        searchBox.forEach((elm) => {
+            elm.addEventListener('click', () => {
+                elm.previousElementSibling.value = '';
+            });
+        });
       if (nowPosition == 'search') {
         window.location.reload()
       } else {
         this.$router.replace('search')
       }
-    }
+    },
   }
 }
 </script>
+
+<style>
+.search-box{
+  display: inline-block;
+  position: relative;
+  border-radius: 50px;
+  border: 5px solid #f1bbba;
+}
+.search-box span {
+  width: 25px;
+  height: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: -13px;
+  right: -15px;
+  transition: bottom 300ms ease-out 300ms, right 300ms ease-out 300ms;
+}
+.search-box span:before, .search-box span:after {
+  content: '';
+  height: 25px;
+  border-left: solid 5px #f1bbba;
+  position: absolute;
+  transform: rotate(-45deg);
+}
+.search-box span:after {
+  transform: rotate(45deg);
+  opacity: 0;
+  top: -20px;
+  right: -10px;
+  transition: top 300ms ease-out, right 300ms ease-out, opacity 300ms ease-out;
+}
+input {
+  font-size: 20px;
+  font-weight: bold;
+  width: 54px;
+  height: 54px;
+  padding: 5px 40px 5px 10px;
+  border: none;
+  box-sizing: border-box;
+  border-radius: 50px;
+  background: #1a1a1a;
+  transition: width 800ms cubic-bezier(0.5, -0.5, 0.5, 0.5) 600ms;
+  color: #f1bbba;
+}
+.search-box input:focus {
+  outline: none;
+}
+.search-box input:focus, .search-box input:not(:placeholder-shown) {
+  width: 300px;
+  transition: width 800ms cubic-bezier(0.5, -0.5, 0.5, 1.5);
+}
+.search-box input:focus + span, .search-box input:not(:placeholder-shown) + span {
+  bottom: 13px;
+  right: 10px;
+  transition: bottom 300ms ease-out 800ms, right 300ms ease-out 800ms;
+}
+.search-box input:focus + span:after, .search-box input:not(:placeholder-shown) + span:after {
+  top: 0;
+  right: 10px;
+  opacity: 1;
+  transition: top 300ms ease-out 1100ms, right 300ms ease-out 1100ms, opacity 300ms ease 1100ms;
+}
+</style>
