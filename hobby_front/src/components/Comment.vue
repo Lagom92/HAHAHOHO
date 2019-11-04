919b86094b95d729814676f2151068c05d9bbbdc
Lagom92 <jyyt0147@gmail.com>
2019-11-05 01:12:33 +0900

    Developing comment btn color

--- hobby_front/src/components/Comment.vue
+++ hobby_front/src/components/Comment.vue
@@ -10,7 +10,7 @@
                     <v-form>
                         <input v-model="text" label="댓글 작성" placeholder="댓글을 남겨주세요">
                         <div class="d-flex justify-end">
-                            <v-btn dark color="light-blue" @click="createComment()">작성</v-btn>
+                            <v-btn dark color="#74B4A0" @click="createComment()">작성</v-btn>
                         </div>
                     </v-form>
                 </v-container>
